ce5ece6fe368d2c5d25ab56bb42d14826bc9c968
juanlfr <fuentes0091@gmail.com>
2019-12-03 19:33:58 +0100

    routes file modification, resources added

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  get 'articles/index'
-  get 'articles/show'
-  get 'articles/new'
-  get 'articles/edit'
-  get 'projects/index'
-  get 'projects/show'
-  get 'projects/new'
-  get 'projects/edit'
+ 
+  resources :articles
+  resources :projects
+  resources :project_categories
+  resources :categories
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
