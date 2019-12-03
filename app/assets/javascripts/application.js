1834a3c0fa115e90f9d8d61fb5a4aa1d680bd18c
kwouix <emma.bock49@gmail.com>
2019-12-03 15:09:24 +0100

    Updated the template : modified requires in applications.css and javascripts.js. Precompiled assets.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,8 @@
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
-// vendor/assets/javascripts directory can be referenced here using a relative path.
+// Any JavaScript/Coffee file within this directory, lib/javascripts, or any plugin's
+// vendor/javascripts directory can be referenced here using a relative path.
 //
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // compiled file. JavaScript code in this file should be added after the last require_* statement.
@@ -14,42 +14,41 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
-//= require jquery3
-//= require assets/style/js/bootstrap.min
-//= require assets/style/js/jquery.min
-//= require assets/style/js/plugins
-//= require assets/style/js/popper.min
-//= require assets/style/js/scripts
-//= require assets/style/revolution/js/jquery.themepunch.enablelog
-//= require assets/style/revolution/js/jquery.themepunch.revolution.min
-//= require assets/style/revolution/js/jquery.themepunch.tools.min
-//= require assets/style/revolution/js/tp-color-picker.min
-//= require assets/style/revolution/js/extensions/revolution.extension.actions.min
-//= require assets/style/revolution/js/extensions/revolution.extension.carousel.min
-//= require assets/style/revolution/js/extensions/revolution.extension.kenburn.min
-//= require assets/style/revolution/js/extensions/revolution.extension.layeranimation.min
-//= require assets/style/revolution/js/extensions/revolution.extension.migration.min
-//= require assets/style/revolution/js/extensions/revolution.extension.navigation.min
-//= require assets/style/revolution/js/extensions/revolution.extension.parallax.min
-//= require assets/style/revolution/js/extensions/revolution.extension.slideanims.min
-//= require assets/style/revolution/js/extensions/revolution.extension.video.min
-//= require assets/style/revolution/js/extensions/source/revolution.extension.actions
-//= require assets/style/revolution/js/extensions/source/revolution.extension.carousel
-//= require assets/style/revolution/js/extensions/source/revolution.extension.kenburn
-//= require assets/style/revolution/js/extensions/source/revolution.extension.layeranimation
-//= require assets/style/revolution/js/extensions/source/revolution.extension.migration
-//= require assets/style/revolution/js/extensions/source/revolution.extension.navigation
-//= require assets/style/revolution/js/extensions/source/revolution.extension.parallax
-//= require assets/style/revolution/js/extensions/source/revolution.extension.slideanims
-//= require assets/style/revolution/js/extensions/source/revolution.extension.video
-//= require assets/style/revolution/js/jquery.themepunch.enablelog
-//= require assets/style/revolution/js/jquery.themepunch.revolution.min
-//= require assets/style/revolution/js/jquery.themepunch.tools.min
-//= require assets/style/revolution/js/tp-color-picker.min
-//= require assets/style/revolution/js/source/jquery.themepunch.enablelog
-//= require assets/style/revolution/js/source/jquery.themepunch.revolution
-//= require assets/style/revolution/js/source/jquery.themepunch.tools.min
-//= require assets/style/revolution/js/source/tp-color-picker
-//= require assets/style/revolution/revolution-addons/filmstrip/js/revolution.addon.filmstrip.min
-//= require assets/style/revolution/revolution-addons/typewriter/js/revolution.addon.typewriter
-//= require assets/style/revolution/revolution-addons/typewriter/js/revolution.addon.typewriter.min
+//= require style/js/bootstrap.min
+//= require style/js/jquery.min
+//= require style/js/plugins
+//= require style/js/popper.min
+//= require style/js/scripts
+//= require style/revolution/js/jquery.themepunch.enablelog
+//= require style/revolution/js/jquery.themepunch.revolution.min
+//= require style/revolution/js/jquery.themepunch.tools.min
+//= require style/revolution/js/tp-color-picker.min
+//= require style/revolution/js/extensions/revolution.extension.actions.min
+//= require style/revolution/js/extensions/revolution.extension.carousel.min
+//= require style/revolution/js/extensions/revolution.extension.kenburn.min
+//= require style/revolution/js/extensions/revolution.extension.layeranimation.min
+//= require style/revolution/js/extensions/revolution.extension.migration.min
+//= require style/revolution/js/extensions/revolution.extension.navigation.min
+//= require style/revolution/js/extensions/revolution.extension.parallax.min
+//= require style/revolution/js/extensions/revolution.extension.slideanims.min
+//= require style/revolution/js/extensions/revolution.extension.video.min
+//= require style/revolution/js/extensions/source/revolution.extension.actions
+//= require style/revolution/js/extensions/source/revolution.extension.carousel
+//= require style/revolution/js/extensions/source/revolution.extension.kenburn
+//= require style/revolution/js/extensions/source/revolution.extension.layeranimation
+//= require style/revolution/js/extensions/source/revolution.extension.migration
+//= require style/revolution/js/extensions/source/revolution.extension.navigation
+//= require style/revolution/js/extensions/source/revolution.extension.parallax
+//= require style/revolution/js/extensions/source/revolution.extension.slideanims
+//= require style/revolution/js/extensions/source/revolution.extension.video
+//= require style/revolution/js/jquery.themepunch.enablelog
+//= require style/revolution/js/jquery.themepunch.revolution.min
+//= require style/revolution/js/jquery.themepunch.tools.min
+//= require style/revolution/js/tp-color-picker.min
+//= require style/revolution/js/source/jquery.themepunch.enablelog
+//= require style/revolution/js/source/jquery.themepunch.revolution
+//= require style/revolution/js/source/jquery.themepunch.tools.min
+//= require style/revolution/js/source/tp-color-picker
+//= require style/revolution/revolution-addons/filmstrip/js/revolution.addon.filmstrip.min
+//= require style/revolution/revolution-addons/typewriter/js/revolution.addon.typewriter
+//= require style/revolution/revolution-addons/typewriter/js/revolution.addon.typewriter.min
