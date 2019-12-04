badd6ec9d3f888ace90297efe5e5d4be98296319
juanlfr <fuentes0091@gmail.com>
2019-12-04 15:19:27 +0100

    seed done, faker added to gemfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+Project.destroy_all
+Article.destroy_all
+Category.destroy_all
+
+
+admin = Admin.create(email: "admin@yopmail.com", password: "thpstrabourg")
+
+
+categories_array = ["Institutionnel", "Communication", "Documentaire", "Animalier", "Photographie", "Animation", "Portrait", "Expérimental", "Pellicule", "Drone"]
+
+t1 = Date.parse("2019-12-31")
+t2 = Date.parse("2022-01-01")
+
+10.times do
+  project = Project.create(
+    title: Faker::Creature::Animal.name,
+    description: "Projet d'exception des animaux dans son cadre naturel. Un instant de magie pure, capturé par nos photographes de renom.",
+    content: Faker::Lorem.paragraph(sentence_count: 10),
+    date: rand(t1..t2),
+    admin_id: admin.id)
+    puts "seed project"
+
+end
+
+
+10.times do
+    article = Article.create(
+      title: Faker::Creature::Animal.name,
+      description: "Article d'exception des animaux dans son cadre naturel. Un instant de magie pure, capturé par nos photographes de renom.",
+      content: Faker::Lorem.paragraph(sentence_count: 20),
+      admin_id: admin.id)
+      puts "seed aritcle"
+
+  end
+
+
+10.times do
+    category = Category.create(
+      category_name: categories_array.sample)
+      puts "seed category"
+
+end
