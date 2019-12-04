792a00ad95d7a090dd823d08f9811081f14997a1
kwouix <emma.bock49@gmail.com>
2019-12-04 19:56:18 +0100

    Modified syntax in pictures_controller

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -2,7 +2,7 @@ class PicturesController < ApplicationController
   def create
     @project_picture = Project.find(params[:project_id])
     @project_picture.pictures.attach(params[:pictures])
-    redirect_to new_project_path(@project_picture))
+    redirect_to new_project_path(@project_picture)
   end
 
   def destroy
