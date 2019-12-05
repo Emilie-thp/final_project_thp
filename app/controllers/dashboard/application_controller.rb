c87a0b05fa7041063113d725b8fd3a2bd9a9eec0
dstrasbg <argudo.daniel@gmail.com>
2019-12-05 14:04:50 +0100

    Projet view in the Admin dashboard

--- app/controllers/dashboard/application_controller.rb
+++ app/controllers/dashboard/application_controller.rb
@@ -2,11 +2,12 @@ module Dashboard
 
 	class ApplicationController < ::ApplicationController
 
-		def show
+		def index
 			@admin = Admin.find(params[:id])
 			@project = Project.find(params[:id])
 			@article = Article.find(params[:id])
 		  end
+
 	end
 
 end
