d986124d3eb0d0442fa2099b5d64500d02ae4e54
juanlfr <fuentes0091@gmail.com>
2019-12-05 19:56:55 +0100

    Edit project page form_with modified

--- app/controllers/dashboard/projects_controller.rb
+++ app/controllers/dashboard/projects_controller.rb
@@ -13,10 +13,10 @@ module Dashboard
 	  def create
 		@project = Project.new(project_params)
 		@project.admin = current_admin
-		 if @project.save
-			redirect_to edit_dashboard_project
+		if @project.save
+			redirect_to(edit_dashboard_project_path(@project))
 		 else
-			new_dashboard_project_path
+			render new_dashboard_project_path
 		 end
 		  
 	  end
