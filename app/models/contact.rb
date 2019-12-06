fc1510bbf7e1723679ffff0e1395aca2477fbaad
juanlfr <fuentes0091@gmail.com>
2019-12-06 16:55:48 +0100

    After_create put on contact model and contact_request method added

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,4 +2,9 @@ class Contact < ApplicationRecord
     validates :email, presence: true
     validates :name, presence: true
     validates :content, presence: true
+    after_create :contact_request
+
+    def contact_request
+        AdminMailer.contact_email(self).deliver_now
+    end
 end
