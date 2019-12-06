05b85eea1de9105f5951e71b8b471d44d3813258
Emilie-thp <emilie.marjollet@orange.fr>
2019-12-06 10:07:59 +0100

    New controller thumbnails for active_storage

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,7 +3,6 @@ class Project < ApplicationRecord
     belongs_to :admin
  		has_many :project_categories
     has_many :categories, through: :project_categories
-    has_one_attached :image
-    has_many_attached :diaporama
+    has_many_attached :pictures
     
 end
