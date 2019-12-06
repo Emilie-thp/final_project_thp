17fb9cd7caa296036065eb82d0446bbbe6b54e66
Emilie-thp <emilie.marjollet@orange.fr>
2019-12-06 11:10:15 +0100

    Form_field for active_storage pictures

--- app/controllers/dashboard/pictures_controller.rb
+++ app/controllers/dashboard/pictures_controller.rb
@@ -1,15 +1,19 @@
-class PicturesController < ApplicationController
-  
-  def create
-    @project = Project.find(params[:project_id])
-    @project.picture.attach(params[:picture])
-    redirect_to edit_dashboard_project_path(@project)
-  end
+module Dashboard
 
-  def destroy
-    @project = Project.find(params[:project_id])
-    @project.picture.purge
-    redirect_to edit_dashboard_project_path(@project)
+  class PicturesController < ApplicationController
+    
+    def create
+      @project = Project.find(params[:project_id])
+      @project.pictures.attach(params[:pictures])
+      redirect_to edit_dashboard_project_path(@project)
+    end
+
+    def destroy
+      @project = Project.find(params[:project_id])
+      @project.picture.spurge
+      redirect_to edit_dashboard_project_path(@project)
+    end
+
   end
 
 end
