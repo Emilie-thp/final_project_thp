fad7e6b603794f7bee0a038405ed2fd26f2c2596
Emilie-thp <emilie.marjollet@orange.fr>
2019-12-06 10:20:43 +0100

    Modified thumbnails and pictures controller

--- app/controllers/dashboard/pictures_controller.rb
+++ app/controllers/dashboard/pictures_controller.rb
@@ -1,13 +1,15 @@
 class PicturesController < ApplicationController
+  
   def create
-    @project_picture = Project.find(params[:project_id])
-    @project_picture.pictures.attach(params[:pictures])
-    redirect_to new_project_path(@project_picture)
+    @project = Project.find(params[:project_id])
+    @project.picture.attach(params[:picture])
+    redirect_to edit_dashboard_project_path(@project)
   end
 
   def destroy
-    @project_picture = Project.find(params[:project_id])
-	  @project_picture.pictures.purge
-	  redirect_to project_path(@project_picture)
+    @project = Project.find(params[:project_id])
+    @project.picture.purge
+    redirect_to edit_dashboard_project_path(@project)
   end
+
 end
