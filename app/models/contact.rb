912df766379b88273e76a22a89ef3067a783feb8
juanlfr <fuentes0091@gmail.com>
2019-12-06 17:47:38 +0100

    Templates ok, validation subject addedto contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,7 @@
 class Contact < ApplicationRecord
     validates :email, presence: true
     validates :name, presence: true
+    validates :subject, presence: true
     validates :content, presence: true
     after_create :contact_request
 
