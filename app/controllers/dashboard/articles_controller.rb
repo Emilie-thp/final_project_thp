02227d0e0af6e753f2a1645dfc31017d49607c02
dstrasbg <argudo.daniel@gmail.com>
2019-12-06 02:15:01 +0100

    Creation of a new article

--- app/controllers/dashboard/articles_controller.rb
+++ app/controllers/dashboard/articles_controller.rb
@@ -11,37 +11,46 @@ module Dashboard
 	    @article = Article.find(params[:id])
 	  end
 
-	  def new
-	    @article = Article.new
-	  end
-
 		def create
 			@article = Article.new(article_params)
 			@article.admin = current_admin
 				if @article.save
 					flash[:notice] = "Un nouvel article a bien été créé (n°#{@article.id})!"
-					redirect_to edit_dashboard_article_path(@article)
+					redirect_to edit_dashboard_articles_path(@article)
 				 else
 					render "new"
 				 end
 			end
 
-	  def edit
+		def new
+			@article = Article.new
+		end
+
+		def edit
+			@article = Article.find(params[:id])
 	  end
 
 	  def update
-	    @article = Article.find(params[:id])
-	    @article.update(article_params)
-	  end
+			@article = Article.find(params[:id])
+			if @article.update(project_params)
+	    	flash[:notice] = "L'article n°#{@article.id} a bien été édité !"
+				redirect_to dashboard_articles_path
+			else
+				render "edit"
+	  	end
+		end
 
 	  def destroy
-	    @article = Article.find(params[:id])
+			@article = Article.find(params[:id])
+			@article.destroy
+    	flash[:notice] = "L'article n°#{@article.id} a bien été supprimé !"
+    	redirect_to dashboard_articles_path
 	  end
 
 	  private
 
 	  def article_params
-	    params.require(:article).permit(:title, :description, :content, :published)
+	    params.require(:article).permit(:title, :description, :content, :date, :published)
 	  end
 
 	end
