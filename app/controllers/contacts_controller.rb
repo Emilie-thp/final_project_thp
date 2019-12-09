fbd2c9580d7b9517a2909ab68a8065be1f7501b9
juanlfr <fuentes0091@gmail.com>
2019-12-09 12:25:53 +0100

    Contact flashes were added

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,6 +7,7 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(contact_params)
       if @contact.save
+        flash[:notice] = "Votre demande a bien été envoyé"
         redirect_to root_path
       else
         render new_contact_path
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,11 +1,12 @@
 class Contact < ApplicationRecord
-    validates :email, presence: true
-    validates :name, presence: true
-    validates :subject, presence: true
-    validates :content, presence: true
+    validates :email, presence: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "Merci d'indiquer une adresse email valide" }
+    validates :name, presence: { message: "Merci d'indiquer votre Prénom et nom sont obligatoires" }
+    validates :subject, presence: { message: "Merci d'indiquer l'objet de la demande" }
+    validates :content, presence: { message: "Merci d'indiquer le message de la demande" }
     after_create :contact_request
 
     def contact_request
         AdminMailer.contact_email(self).deliver_now
     end
 end
+    
