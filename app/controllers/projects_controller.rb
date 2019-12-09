9069d08ddf5adc19980c3933fbe4d821b017970d
Emilie-thp <emilie.marjollet@orange.fr>
2019-12-09 22:44:49 +0100

    Added comment to projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,12 +2,15 @@ class ProjectsController < ApplicationController
 
   def index
     @categories = Category.all
+
+    #if condition is here to sort projects out regarding their categories (params :category in view)
     if params.has_key?(:category)
     	@category = Category.find_by(category_name: params[:category])
     	@projects = @category.projects
     else
  		  @projects = Project.all
  		end
+ 		
   end
 
   def show
