9d07833c2d7e34007edd8099482c6ab679a6638b
dstrasbg <argudo.daniel@gmail.com>
2019-12-09 18:25:23 +0100

    Event creation model

---
+++ db/migrate/20191209172236_create_events.rb
@@ -0,0 +1,14 @@
+class CreateEvents < ActiveRecord::Migration[5.2]
+  def change
+    create_table :events do |t|
+      t.string :title
+      t.string :description
+      t.text :content
+      t.datetime :date
+      t.string :location
+      t.boolean :published
+
+      t.timestamps
+    end
+  end
+end
