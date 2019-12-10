f7029ef18984b125e0e2234e75ec5e296b6a26e5
juanlfr <55744110+juanlfr@users.noreply.github.com>
2019-12-10 11:53:39 +0100

    Update project.rb

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,7 @@
 class Project < ApplicationRecord
 
   validates :title, presence: {message: "Merci d'indiquer le titre de la réalisation"}, length: { minimum: 2, message: "Le titre doit avoir au moins 2 caractères" }
-  validates :description, presence: {message: "Merci d'indiquer la description de la réalisation"}, length: { maximum: 200, message: "La description doit avoir 200 catactères maximum" }
+  validates :description, presence: {message: "Merci d'indiquer la description de la réalisation"}, length: { maximum: 200, message: "La description doit avoir 200 caractères maximum" }
   validates :content, presence: {message: "Merci d'indiquer le contenu de la réalisation"}
   
   belongs_to :admin
