8d00e5ec0ba3b9a0424f40d72210e659e7722d92
juanlfr <fuentes0091@gmail.com>
2019-12-10 11:45:13 +0100

    Event errors translated

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,8 @@
 class Event < ApplicationRecord
 
-  validates :title, presence: true, length: { minimum: 2 }
-  validates :description, presence: true, length: { maximum: 200 }
-  validates :content, presence: true
+  validates :title, presence: {message: "Merci d'indiquer le titre de la réalisation"}, length: { minimum: 2, message: "Le titre doit avoir au moins 2 caractères" }
+  validates :description, presence: {message: "Merci d'indiquer la description de la réalisation"}, length: { maximum: 200, message: "La description doit avoir 200 catactères maximum" }
+  validates :content, presence: {message: "Merci d'indiquer le contenu de la réalisation"}
   
       belongs_to :admin
 
