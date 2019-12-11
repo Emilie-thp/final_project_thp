374dd16a295bd6b7affd672ef23b4b88b1d25c02
Emilie-thp <emilie.marjollet@orange.fr>
2019-12-11 23:35:38 +0100

    Changed flash information

--- app/controllers/dashboard/events_controller.rb
+++ app/controllers/dashboard/events_controller.rb
@@ -21,8 +21,8 @@ module Dashboard
 			@event.admin = current_admin
 
 			if @event.save
-	    	flash[:notice] = "Une nouvelle actualité a bien été créée (n°#{@event.id})!"
-				redirect_to dashboard_events_path
+	    	flash[:notice] = "Vous venez de créer une nouvelle actualité. Vérifiez les informations avant de la mettre en ligne !"
+				redirect_to edit_dashboard_events_path(@event)
 		 	else
 				render "new"
 		 	end
@@ -39,7 +39,7 @@ module Dashboard
 	  def update
 	    @event = Event.find(params[:id])
 	    if @event.update(event_params)
-	    	flash[:notice] = "L'actualité n°#{@event.id} a bien été éditée !"
+	    	flash[:notice] = "L'actualité '#{@event.title}'' a bien été éditée !"
 				redirect_to dashboard_events_path
 			else
 				render "edit"
@@ -64,7 +64,7 @@ module Dashboard
 	  def destroy
 	    @event = Event.find(params[:id])
 	    @event.destroy
-    	flash[:notice] = "L'actualité n°#{@event.id} a bien été supprimée !"
+    	flash[:notice] = "L'actualité '#{@event.title}'' a bien été supprimée !"
     	redirect_to dashboard_events_path
 	  end
 
