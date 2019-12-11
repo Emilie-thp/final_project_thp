84ca7d06567d3cdbc5a7380037b86d84b4443504
Emilie-thp <emilie.marjollet@orange.fr>
2019-12-11 23:35:50 +0100

    Changed flash information

--- app/controllers/dashboard/projects_controller.rb
+++ app/controllers/dashboard/projects_controller.rb
@@ -26,7 +26,7 @@ module Dashboard
 			@project.admin = current_admin
 			
 			if @project.save
-	    	flash[:notice] = "Une nouvelle réalisation a bien été créée (n°#{@project.id})! Merci d'ajouter les photos avant de la publier."
+	    	flash[:notice] = "Vous venez de créer une nouvelle réalisation ! Ajoutez des photos et relisez les informations avant de la mettre en ligne !"
 				redirect_to edit_dashboard_project_path(@project)
 		 	else
 				render "new"
@@ -40,7 +40,7 @@ module Dashboard
 	  def update
 	    @project = Project.find(params[:id])
 	    if @project.update(project_params)
-	    	flash[:notice] = "La réalisation n°#{@project.id} a bien été éditée !"
+	    	flash[:notice] = "La réalisation '#{@project.title}' a bien été éditée !"
 				redirect_to dashboard_projects_path
 			else
 				render "edit"
@@ -50,7 +50,7 @@ module Dashboard
 	  def destroy
 	    @project = Project.find(params[:id])
 	    @project.destroy
-    	flash[:notice] = "La réalisation n°#{@project.id} a bien été supprimée !"
+    	flash[:notice] = "La réalisation '#{@project.title}' a bien été supprimée !"
     	redirect_to dashboard_projects_path
 	  end
 
@@ -78,7 +78,7 @@ module Dashboard
 		@project = Project.find(params[:id])
 		@admin = Admin.find(@project.admin_id)
 			unless @admin.id == current_admin.id
-				flash[:notice] = "Vous n'avez pas le droit d'éditer ou supprimer le projet car vous n'êtes pas l'auteur !"
+				flash[:notice] = "Vous n'avez pas le droit d'éditer ou supprimer cette réalisation car vous n'êtes pas l'auteur !"
 				redirect_to dashboard_projects_path
 			end
 		end
