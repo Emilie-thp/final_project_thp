ad5ecbe32de56b242a4ca043a71b1a9da9f734e3
juanlfr <fuentes0091@gmail.com>
2019-12-11 15:34:02 +0100

    Categories added to create method

--- app/controllers/dashboard/projects_controller.rb
+++ app/controllers/dashboard/projects_controller.rb
@@ -20,7 +20,7 @@ module Dashboard
 	  def create
 			@project = Project.new(project_params)
 			@project.admin = current_admin
-			
+			@project.categories << params[:categories]
 			if @project.save
 	    	flash[:notice] = "Une nouvelle réalisation a bien été créée (n°#{@project.id})! Merci d'ajouter les photos avant de la publier."
 				redirect_to edit_dashboard_project_path(@project)
