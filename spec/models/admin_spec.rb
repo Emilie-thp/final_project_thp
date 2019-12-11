07ab74a7a7a6a2cae25b88e1c10a7c47babb21ec
dstrasbg <argudo.daniel@gmail.com>
2019-12-11 20:27:10 +0100

    Valid User model tests by rspec

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -2,9 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Admin, type: :model do
   
-  #pending "add some examples to (or delete) #{__FILE__}"
-
-  before(:each) do @admin = Admin.create(first_name: "John", last_name: "Doe", pseudo: "jojo", speciality: "carpenter", email: "wwww@mail.com", password:"gagaga", display:"false", description:"juratime")
+  before(:each) do @admin = Admin.create(first_name: "John", last_name: "Doe", pseudo: "jojo", speciality: "Carpenter", email: "wwww@mail.com", password:"gagaga", display:"false", description:"bonhomme")
   end
 
   context "validation" do
@@ -14,23 +12,6 @@ RSpec.describe Admin, type: :model do
       expect(@admin).to be_valid
     end
 
-    describe "#first_name" do
-      it "should not be valid without first_name" do
-        bad_admin = Admin.create(last_name: "Doe")
-        expect(bad_admin).not_to be_valid
-        # test très sympa qui permet de vérifier que la fameuse formule admin.errors retourne bien un hash qui contient une erreur concernant le first_name. 
-        expect(bad_admin.errors.include?(:first_name)).to eq(true)
-      end
-    end
-
-    describe "#last_name" do
-      it "should not be valid without last_name" do
-        bad_admin = Admin.create(first_name: "John")
-        expect(bad_admin).not_to be_valid
-        expect(bad_admin.errors.include?(:last_name)).to eq(true)
-      end
-    end
-
     describe "#pseudo" do
       it "should not be lower that 3 characters" do
         invalid_admin = Admin.create(first_name: "John", last_name: "Doe", pseudo: "a")
@@ -38,38 +19,15 @@ RSpec.describe Admin, type: :model do
         expect(invalid_admin.errors.include?(:pseudo)).to eq(true)
       end
     end
-
-
   end
 
   context "associations" do
 
     describe "projects" do
       it "should have_many projets" do
-        project = Project.create(title: "strtrtr", description:"cscs", content:"fafa", admin: @admin)
+        project = Project.create(title: "Vidéoblog", description:"Foret", content:"chouette", admin: @admin)
         expect(@admin.projects.include?(project)).to eq(true)
       end
     end
-
   end   
-
-  context "public instance methods" do
-
-    describe "#full_name" do
-
-      it "should return a string" do
-        expect(@admin.full_name).to be_a(String)
-      end
-
-      it "should return the full name" do
-        admin_1 = Admin.create(first_name: "John", last_name: "Doe", pseudo: "johndoe")
-        expect(admin_1.full_name).to eq("John Doe")
-        admin_2 = Admin.create(first_name: "Jean-Michel", last_name: "Durant", pseudo: "kikou_du_67")
-        expect(admin_2.full_name).to eq("Jean-Michel Durant")
-      end
-    end
-
-  end
-
-
 end
