cd638f8f00ee20011320cd153dd1cebbf10dfb2f
Emilie-thp <emilie.marjollet@orange.fr>
2019-12-14 17:31:38 +0100

    Cleaned app/models/contact.rb

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,14 +1,14 @@
 class Contact < ApplicationRecord
-    
-  validates :email, presence: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "Merci d'indiquer une adresse email valide" }
-  validates :name, presence: { message: "Merci d'indiquer votre nom et prénom" }
-  validates :subject, presence: { message: "Merci d'indiquer l'objet de la demande" }
-  validates :content, presence: { message: "Merci de compléter le contenu du message" }
+
+  validates :email, presence: true, format: {with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "Merci d'indiquer une adresse email valide."}
+  validates :name, presence: {message: "Merci d'indiquer votre nom et prénom."}
+  validates :subject, presence: {message: "Merci d'indiquer l'objet de la demande."}
+  validates :content, presence: {message: "Merci de compléter le contenu du message."}
   after_create :contact_request
 
   def contact_request
-      AdminMailer.contact_email(self).deliver_now
+    AdminMailer.contact_email(self).deliver_now
   end
-  
+
 end
     
