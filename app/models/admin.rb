bb0d0a46b8e7bba70609733f61c67cba0dbe623f
Emilie-thp <emilie.marjollet@orange.fr>
2019-12-14 21:08:41 +0100

    Added validation to admin model for pseudo

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -4,13 +4,13 @@ class Admin < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :pseudo, presence: true, length: {minimum: 2}
+  validates :pseudo, presence: {message: "Merci d'indiquer un pseudo."}, length: {minimum: 2}
 
   has_many :projects, dependent: :delete_all
   has_many :articles, dependent: :delete_all
   has_many :events, dependent: :delete_all
   has_one_attached :avatar, dependent: :delete_all
-  
+
   after_create :welcome_send
 
   def welcome_send
