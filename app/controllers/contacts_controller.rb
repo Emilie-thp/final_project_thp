16750761addb57305fa42f5ddc6ff89b47eee26f
Emilie-thp <emilie.marjollet@orange.fr>
2019-12-14 17:20:13 +0100

    Cleaned app/controllers/contacts_controller.rb

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,19 +6,19 @@ class ContactsController < ApplicationController
 
   def create
     @contact = Contact.new(contact_params)
-      if @contact.save
-        flash[:notice] = "Votre message a bien été envoyé"
-        redirect_to new_contact_path
-      else
-        render new_contact_path
-      end
+    if @contact.save
+      flash[:notice] = "Votre message a bien été envoyé. Nous vous répondons par email dès que possible."
+      redirect_to new_contact_path
+    else
+      render new_contact_path
+    end
   end
 
 
   private
 
-    def contact_params
-      params.require(:contact).permit(:email, :name, :status, :content, :subject)
-    end
+  def contact_params
+    params.require(:contact).permit(:email, :name, :status, :content, :subject)
+  end
 
 end
