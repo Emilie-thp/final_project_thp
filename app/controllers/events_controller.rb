5c0bb418bd92c5b05c3010c9ba2e52f7fc023933
Emilie-thp <emilie.marjollet@orange.fr>
2019-12-14 17:20:27 +0100

    CLeaned app/controllers/events_controller.rb

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,7 @@
 class EventsController < ApplicationController
-  
+
   def index
-    @events = Event.where(published:true).order('created_at DESC')
+    @events = Event.where(published: true).order('created_at DESC')
   end
 
   def show
