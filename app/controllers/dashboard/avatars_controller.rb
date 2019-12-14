adaa34da35cb8450cfe71cb1d8417508612ed873
Emilie-thp <emilie.marjollet@orange.fr>
2019-12-14 17:58:51 +0100

    Cleaned app/controllers/dashboard/avatars_controller.rb

--- app/controllers/dashboard/avatars_controller.rb
+++ app/controllers/dashboard/avatars_controller.rb
@@ -1,17 +1,19 @@
-module Dashboard 
+module Dashboard
+
   class AvatarsController < ApplicationController
-    
-		def create
-	    @admin = Admin.find(params[:admin_id])
-	    @admin.avatar.attach(params[:avatar])
-	    redirect_to edit_dashboard_admin_path(@admin)
-	  end
 
-	  def destroy
-	    @admin = Admin.find(params[:admin_id])
-		  @admin.avatar.purge
-		  redirect_to edit_dashboard_admin_path(@admin)
-	  end
+    def create
+      @admin = Admin.find(params[:admin_id])
+      @admin.avatar.attach(params[:avatar])
+      redirect_to edit_dashboard_admin_path(@admin)
+    end
 
-  end
+    def destroy
+      @admin = Admin.find(params[:admin_id])
+      @admin.avatar.purge
+      redirect_to edit_dashboard_admin_path(@admin)
+    end
+
+	end
+
 end
