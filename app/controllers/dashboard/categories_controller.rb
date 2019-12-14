13ccfd477b8dae39f89d069a0c2221fa3e69d8c3
Emilie-thp <emilie.marjollet@orange.fr>
2019-12-14 17:59:00 +0100

    Cleaned app/controllers/dashboard/categories_controller.rb

--- app/controllers/dashboard/categories_controller.rb
+++ app/controllers/dashboard/categories_controller.rb
@@ -1,20 +1,20 @@
 module Dashboard
 
-	class CategoriesController < ApplicationController
+  class CategoriesController < ApplicationController
     before_action :authenticate_admin!
 
-		def index
-			@categories = Category.all.order('created_at DESC')
-		end
+    def index
+      @categories = Category.all.order('created_at DESC')
+    end
 
     def show
       @category = Category.find(params[:id])
     end
 
-	  def new
-      @category = Category.new  
+    def new
+      @category = Category.new
     end
-  
+
     def create
       @category = Category.new(category_params)
       if @category.save
@@ -24,34 +24,34 @@ module Dashboard
         render "new"
       end
     end
-  
-    def edit 
+
+    def edit
       @category = Category.find(params[:id])
-    end 
+    end
 
     def update
       @category = Category.find(params[:id])
-        if @category.update(category_params)
-          flash[:notice] = "La catégorie #{@category.category_name} a bien été mise à jour !"
-          redirect_to dashboard_categories_path
-        else
-          render "edit"
-        end
+      if @category.update(category_params)
+        flash[:notice] = "La catégorie #{@category.category_name} a bien été mise à jour !"
+        redirect_to dashboard_categories_path
+      else
+        render "edit"
+      end
     end
-  
+
     def destroy
       @category = Category.find(params[:id])
       @category.destroy
       respond_to do |format|
-				format.html do
+        format.html do
           flash[:notice] = "La catégorie a bien été supprimée !"
           redirect_to dashboard_categories_path
-				end
-				format.js do
-				end
-			end
-      
-  	end
+        end
+        format.js do
+        end
+      end
+
+    end
 
 
     private
@@ -60,8 +60,6 @@ module Dashboard
       params.require(:category).permit(:category_name)
     end
 
-
-
-	end
+  end
 
 end
